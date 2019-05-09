e666e6db3e03d8eacd208f307ef6748b8167caa9
김세중 <sjkim2@iparking.co.kr>
2019-05-09 14:50:28 +0900

    코드 추가

---
+++ src/main/java/com/github/sejoung/controller/Controller.java
@@ -0,0 +1,15 @@
+package com.github.sejoung.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+
+	@GetMapping("/test")
+	public void test(){
+
+	}
+
+}

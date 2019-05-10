beb33ed2a19014dd1ac229f6de290b580adfaaa7
김세중 <sjkim2@iparking.co.kr>
2019-05-10 10:53:39 +0900

    OOM 발생 시킬수 있는 코드

--- src/main/java/com/github/sejoung/Application.java
+++ src/main/java/com/github/sejoung/Application.java
@@ -1,14 +1,13 @@
 package com.github.sejoung;
 
-import java.lang.reflect.Field;
-import java.nio.charset.Charset;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
 }
--- src/main/java/com/github/sejoung/controller/Controller.java
+++ src/main/java/com/github/sejoung/controller/Controller.java
@@ -1,15 +1,24 @@
 package com.github.sejoung.controller;
 
+import java.util.Stack;
+import java.util.stream.IntStream;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class Controller {
 
-
 	@GetMapping("/test")
-	public void test(){
-
+	public void test() {
+		var stack = new Stack<String>();
+		int mb = 1024 * 1024;
+		var runtime = Runtime.getRuntime();
+		log.info("Free Memory = {} Max Memory = {} totalMemory = {}",
+				runtime.freeMemory() / mb, runtime.maxMemory() / mb,
+				runtime.totalMemory() / mb);
+		IntStream.range(1, Integer.MAX_VALUE).forEach((i) -> stack.push("마구닥 들어가라" + i));
 	}
 
 }
